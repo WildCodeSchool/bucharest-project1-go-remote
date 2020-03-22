2d6d12cdd623e4372633018e79be960bb7126c60
Ioana Alexandra Filip <ioannaf.@192-168-0-116.rdsnet.ro>
2020-03-22 21:50:30 +0200

    cards html + css, mobile nav 1st v

---
+++ mobileNav.js
@@ -0,0 +1,37 @@
+
+
+function myFunction() {
+    var x = document.getElementById("my-main-header");
+    if (x.className === "main-header") {
+      x.className += " responsive";
+    } else {
+      x.className = "main-header";
+    }
+  }
+
+
+
+
+
+
+
+
+// function myFunction() {
+//     const x = document.getElementsByClass("nav-link");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+//   }
+
+
+// x.addEventListener('click', function navFunction(){
+//     if (x.style.display === "block"){x.style.display = 'none';} 
+//     else {x.style.display = 'block';}
+// } )
+
+
+// function myFunction() {
+//     var x = document.getElementsByClassName("nav-link");
+//     x.style.display = 'block';}
