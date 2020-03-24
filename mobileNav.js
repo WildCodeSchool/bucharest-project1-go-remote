3f267d291c0eb5d0be3862136e856c326a08b8b7
Ioana Alexandra Filip <ioannaf.@192-168-0-116.rdsnet.ro>
2020-03-24 19:24:02 +0200

    mobile nav bar v2. x

--- mobileNav.js
+++ mobileNav.js
@@ -2,10 +2,13 @@
 
 function myFunction() {
     var x = document.getElementById("my-main-header");
-    if (x.className === "main-header") {
+    var y = document.getElementById("my-mob-menu")
+    if (x.className === "main-header" && y.className === "mob-menu") {
       x.className += " responsive";
+      y.className += " change ";
     } else {
       x.className = "main-header";
+      y.className = "mob-menu"
     }
   }
 
