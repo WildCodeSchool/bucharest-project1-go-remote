32b396cf0e47442daa380122242eb7a7513ad706
Ioana Alexandra Filip <ioannaf.@192-168-0-116.rdsnet.ro>
2020-03-27 08:48:40 +0200

    added alt to the sec pages for the logo

--- mobileNav.js
+++ mobileNav.js
@@ -19,22 +19,3 @@ function myFunction() {
 
 
 
-// function myFunction() {
-//     const x = document.getElementsByClass("nav-link");
-//     if (x.style.display === "block") {
-//       x.style.display = "none";
-//     } else {
-//       x.style.display = "block";
-//     }
-//   }
-
-
-// x.addEventListener('click', function navFunction(){
-//     if (x.style.display === "block"){x.style.display = 'none';} 
-//     else {x.style.display = 'block';}
-// } )
-
-
-// function myFunction() {
-//     var x = document.getElementsByClassName("nav-link");
-//     x.style.display = 'block';}
